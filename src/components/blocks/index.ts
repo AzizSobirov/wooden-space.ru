ad96997b8d7b77a10f2ac25f5273e537c4e9bcc0
Aziz Sobirov <azizsobirovhazarasp@gmail.com>
2025-02-19 13:18:03 +0500

    19.02.2025

--- src/components/blocks/index.ts
+++ src/components/blocks/index.ts
@@ -14,3 +14,4 @@ export { default as Price } from "./Price.astro";
 export { default as OurExpertise } from "./OurExpertise.astro";
 export { default as History } from "./History.astro";
 export { default as Painting } from "./Painting.astro";
+export { default as Advantages } from "./Advantages.astro";
